f1b84196dbf649b0fa0ce7d6f6c2a1835d8f3eaf
Struan Donald <struan@exo.org.uk>
2013-01-28 18:00:32 +0000

    move create report button down on front page map as it interferes less with scrolling about

--- www/js/map-OpenLayers.js
+++ www/js/map-OpenLayers.js
@@ -210,9 +210,10 @@ function show_map(){
         $('#mark-here').show();
         markHere = $('#mark-here');
         var newX = $(window).width() / 2 - markHere.width() / 2;
+        var markY = $(window).height() * 6 / 8 - markHere.height() / 2;
         var newY = $(window).height() * 4 / 6 - markHere.height() / 2;
         markHere.css({
-            top: newY + "px"
+            top: markY + "px"
         });
         $('#use-location').css({
             top: newY + "px"
