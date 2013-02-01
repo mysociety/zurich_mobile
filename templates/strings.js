b2e5e7063c427290c15929cec4e458e323095b8c
Struan Donald <struan@exo.org.uk>
2013-02-01 12:59:08 +0000

    source templates for generating translated files

---
+++ templates/strings.js
@@ -0,0 +1,13 @@
+var STRINGS = {
+    location_error: '[% loc('Location error') %]',
+    location_problem: '[% loc('There was a problem looking up your location') %]',
+    multiple_locations: '[% loc('More than one location matched that name') %]',
+    sync_error: '[% loc('Sync error') %]',
+    report_send_error: '[% loc('There was a problem submitting your report. Please try again') %]',
+    missing_location: '[% loc('Please enter a location') %]',
+    location_check_failed: '[% loc('Could not check your location') %]',
+    geolocation_failed: '[% loc('Could not determine your location') %]',
+    select_category: '[% loc('-- Pick a categoy --') %]',
+    required: '[% loc('required') %]',
+    invalid_email: '[% loc('Invalid email') %]'
+};
