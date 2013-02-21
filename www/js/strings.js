e739947f216b3dfa15d90967c9ad018046b7fb49
Struan Donald <struan@exo.org.uk>
2013-02-21 11:54:28 +0000

    updated translations

--- www/js/strings.js
+++ www/js/strings.js
@@ -4,13 +4,13 @@ var STRINGS = {
     multiple_locations: 'Mehrere Adressen gefunden',
     sync_error: 'Sync Fehler',
     report_send_error: 'Ihre Meldung konnte nicht übermittelt werden. Bitte versuchen Sie es nochmals.',
-    missing_location: 'Ungefähre Adresse de Mangels',
+    missing_location: 'Geben Sie die ungefähre Adresse des Mangels an',
     location_check_failed: 'Ortung fehlgeschlagen',
     geolocation_failed: 'Ihre Position konnte nicht bestimmt werden',
     select_category: '-- Wählen Sie eine Kategorie --',
     required: 'benötigt',
     invalid_email: 'Ungültiges E-Mail',
     photo_failed: 'Fehler bei Foto',
-    no_connection: 'No network connection available for submitting your report. Please try again later',
-    more_details: 'Beschreiben'
+    no_connection: 'Keine Internetverbindung gefunden. Bitte versuchen Sie es später nochmals.',
+    more_details: 'Beschreibung'
 };
