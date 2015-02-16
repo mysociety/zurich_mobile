0ac07c1ea062a68d68d162c3ecdbba7e9da3e4c9
Dave Arter <davea@mysociety.org>
2015-02-16 19:52:22 +0000

    Allow Report model to upload up to 3 photos to the backend
    This is a bit of an abuse of the FileTransfer plugin, which technically
    doesn't support uploading more than one file at a time. There's no good
    reason for that though, the HTTP spec allows an arbitrary number of files in a
    POST request.

--- www/js/models.js
+++ www/js/models.js
@@ -30,7 +30,9 @@
                 category: '',
                 phone: '',
                 pc: '',
-                file: ''
+                file: '',
+                file2: '',
+                file3: ''
             },
 
             sync: function(method, model, options) {
@@ -67,6 +69,76 @@
                     };
                 }
                 return false;
+            },
+
+            _readFileAsBase64String: function(file, success, error) {
+                return this._readFileAsBinaryString(file, function(data) {
+                    var b64 = btoa(data);
+                    success(b64);
+                }, error);
+            },
+
+            _readFileAsBinaryString: function(file, success, error) {
+                var reader = new FileReader();
+                reader.onloadend = function() {
+                    success(this.result);
+                };
+                reader.onerror = error;
+                return reader.readAsBinaryString(file);
+            },
+
+            _getParamName: function(field, encoding, length) {
+                // The FileTransfer plugin technically only supports a single
+                // file in each upload. However, we can force other files to
+                // be added with a little workaround.
+                // FileTransfer allows extra parameters to be sent with the
+                // HTTP POST request, each of which is its own part of the
+                // multipart-encoded request.
+                // For a part to be treated as a file by the backend we need
+                // to provide a 'filename' value in the Content-Disposition
+                // header. The FileTransfer code doesn't escape the names of
+                // extra POST parameters[0][1], so we can take advantage of this
+                // and essentially inject our own header lines and filename
+                // value with a carefully-crafted HTTP POST field name that's
+                // passed to FileTransfer.upload.
+                // FIXME: This is basically a hack, and needs a better
+                // solution at some point.
+                // [0]: https://github.com/apache/cordova-plugin-file-transfer/blob/49c21f951f51381d887646b38823222ed11c60c1/src/ios/CDVFileTransfer.m#L208
+                // [1]: https://github.com/apache/cordova-plugin-file-transfer/blob/49c21f951f51381d887646b38823222ed11c60c1/src/android/FileTransfer.java#L369
+                var name = field + '"; filename="' + field + '.jpg"\r\n';
+                name += "Content-Type: image/jpeg\r\n";
+                name += "Content-Transfer-Encoding: " + encoding + "\r\n";
+                name += "Content-Length: " + length + "\r\n";
+                name += 'X-Ignore-This-Header: "'; // to close the open quotes
+                return name;
+            },
+
+            _addExtraPhotos: function(model, options, success, error) {
+                var photos = [];
+                if ( model.get('file2') && model.get('file2') !== '' ) {
+                    photos.push({field: "photo2", uri: model.get('file2')});
+                }
+                if ( model.get('file3') && model.get('file3') !== '' ) {
+                    photos.push({field: "photo3", uri: model.get('file3')});
+                }
+                this._addNextExtraPhoto(photos, options, success, error);
+            },
+
+            _addNextExtraPhoto: function(photos, options, success, error) {
+                var photo = photos.shift();
+                if (photo === undefined) {
+                    success();
+                    return;
+                }
+                var self = this;
+                resolveLocalFileSystemURL(photo.uri, function(fileentry) {
+                    fileentry.file(function(file) {
+                        self._readFileAsBase64String(file, function(data) {
+                            options.params[self._getParamName(photo.field, "base64", data.length)] = data;
+                            self._addNextExtraPhoto(photos, options, success, error);
+                        }, error);
+                    }, error);
+                }, error);
             },
 
             post: function(model,options) {
@@ -129,9 +201,19 @@
                     options.params = params;
                     options.chunkedMode = false;
 
-                    $('#ajaxOverlay').show();
-                    var ft = new FileTransfer();
-                    ft.upload(fileURI, CONFIG.FMS_URL + "report/new/mobile", fileUploadSuccess, fileUploadFail, options);
+                    // If file2 or file3 have been set on this model we need to
+                    // add the photos to the file upload request manually
+                    // as FileTransfer only supports a single file upload.
+                    this._addExtraPhotos(
+                        model,
+                        options,
+                        function() {
+                            $('#ajaxOverlay').show();
+                            var ft = new FileTransfer();
+                            ft.upload(fileURI, CONFIG.FMS_URL + "report/new/mobile", fileUploadSuccess, fileUploadFail, options);
+                        },
+                        fileUploadFail
+                    );
                 } else {
                     $.ajax( {
                         url: CONFIG.FMS_URL + "report/new/mobile",
