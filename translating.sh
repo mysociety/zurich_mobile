defc57632966c2e0495f724c0c5f35d299c5fc5b
Dave Arter <davea@mysociety.org>
2019-07-08 16:16:45 +0100

    Make sure translating.sh calls cron-wrapper

--- translating.sh
+++ translating.sh
@@ -5,10 +5,10 @@ set -e
 
 FIXMYSTREET_REPO=${1:-"../fixmystreet"}
 
-./bin/gettext-extract
+$FIXMYSTREET_REPO/bin/cron-wrapper ./bin/gettext-extract
 ./bin/gettext-merge
 "$FIXMYSTREET_REPO/commonlib/bin/gettext-makemo" ZurichMobileApp
-perl -I"$FIXMYSTREET_REPO/commonlib/perllib" -I"$FIXMYSTREET_REPO/local"  bin/localise_templates
+$FIXMYSTREET_REPO/bin/cron-wrapper perl -I"$FIXMYSTREET_REPO/commonlib/perllib" -I"$FIXMYSTREET_REPO/local"  bin/localise_templates
 
 cp compiled/*.html www/templates
 cp compiled/*.js www/js
